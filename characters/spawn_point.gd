9f0966aa5532379f4df0c1996e8917fb7a6c69d9
GuillermoPape <guillermo.pape@outlook.com>
2025-04-14 14:21:24 +0100

    spawnpoints

--- characters/spawn_point.gd
+++ characters/spawn_point.gd
@@ -1,4 +1,20 @@
 extends Node2D
 
-func _ready() -> void:
-	pass
+@export var enemy_scene: PackedScene
+
+func _on_spawn_timer_timeout() -> void:
+	var enemy = enemy_scene.instantiate()
+	get_tree().root.add_child(enemy)
+	enemy.global_position = global_position
+	
+var enabled = false	
+func turn_on():
+	enabled = true
+	$SpawnTimer.start()
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	$SpawnTimer.stop()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	if enabled:
+		$SpawnTimer.start()
