eb4285c9452f35d9af2804c48f9fc3d0265e4068
Meine Velzel <m.velzel@hotmail.com>
2025-04-14 15:29:47 +0200

    Fix spawn logic

--- characters/spawn_point.gd
+++ characters/spawn_point.gd
@@ -8,13 +8,17 @@ func _on_spawn_timer_timeout() -> void:
 	enemy.global_position = global_position
 	
 var enabled = false	
+var screen_visible = true
 func turn_on():
 	enabled = true
-	$SpawnTimer.start()
+	if not screen_visible:
+		$SpawnTimer.start()
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	screen_visible = true
 	$SpawnTimer.stop()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	screen_visible = false
 	if enabled:
 		$SpawnTimer.start()
