e1588b5e331fc59e0f0f5ce83a777f68998bb5f6
GuillermoPape <guillermo.pape@outlook.com>
2025-04-14 09:06:39 +0100

    Caracter takes damage

--- characters/character.gd
+++ characters/character.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+@export var health = 500
 @export var SPEED = 300.0
+@export var sprite: Sprite2D
 
 func _physics_process(_delta: float) -> void:
 	var direction = Vector2(
@@ -23,3 +25,33 @@ func _process(_delta: float) -> void:
 	var mouse_position = get_local_mouse_position()
 	indicator_node.position = mouse_position.normalized() * INDICATOR_DISTANCE
 	indicator_node.rotation = Vector2.ZERO.direction_to(mouse_position).angle()
+
+var enemy_contact_list: Array[Node2D] = []
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemy"):
+		enemy_contact_list.append(body)
+		timer_take_damage(body)
+		
+
+const DAMAGE_DELAY = 0.75
+func timer_take_damage(body: Node2D):
+	health = health - 50
+	
+	sprite.self_modulate = Color(1,0,0)
+	$DamageFlash.start()
+	
+	if health <= 0:
+		get_tree().reload_current_scene()
+		return
+	
+	await get_tree().create_timer(DAMAGE_DELAY).timeout
+	if enemy_contact_list.find(body) != -1:
+		timer_take_damage(body)
+	
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Enemy"):
+		enemy_contact_list.remove_at(enemy_contact_list.find(body))
+
+
+func _on_damage_flash_timeout() -> void:
+	sprite.self_modulate = Color(1,1,1)
