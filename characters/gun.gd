3f010a7fc77100664aac79f1d43974ebe7ff5d26
Maurice <mauricetulp@live.nl>
2025-04-15 13:29:36 +0200

    Gun shot sound effect

--- characters/gun.gd
+++ characters/gun.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var enabled = false
 
+@onready var gun_shot_sound_resource: AudioStream = preload("res://sounds/gun-shot.mp3")
+
 @export var bullet_scene: PackedScene
 @export var bullet_speed = 75
 
@@ -23,3 +25,18 @@ func _unhandled_input(event) -> void:
 		var direction = Vector2.RIGHT.rotated(global_rotation)
 		var velocity = direction * bullet_speed
 		bullet.set_initial_velocity(velocity)
+		
+		_play_gun_shot_sound()
+
+func _play_gun_shot_sound() -> void:
+	var temp_audio_player = AudioStreamPlayer2D.new()
+	temp_audio_player.stream = gun_shot_sound_resource
+	
+	add_child(temp_audio_player)
+	
+	temp_audio_player.play()
+	
+	var sound_length = temp_audio_player.stream.get_length()
+	
+	await get_tree().create_timer(sound_length).timeout
+	temp_audio_player.queue_free()
