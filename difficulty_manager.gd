4ec276334201d365acfd608b7a0dc942de7c7518
Maurice <mauricetulp@live.nl>
2025-04-16 13:45:58 +0200

    Background music

--- difficulty_manager.gd
+++ difficulty_manager.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 var difficulty_stages: Array[int] = [0]
 
+func _ready() -> void:
+	$BackgroundMusicExploration.play()
+
 func add_difficulty_stage(stage: int):
 	difficulty_stages.append(stage)
+	
+	if stage > 0 && !$BackgroundMusicActionFight.playing:
+		$BackgroundMusicExploration.stop()
+		$BackgroundMusicActionFight.play()
 	
 func get_difficulty_stages() -> Array[int]:
 	return difficulty_stages
