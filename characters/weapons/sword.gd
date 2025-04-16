ea4d77e290f4f94d370272df93b0d86a1a1d2dbb
Meine Velzel <m.velzel@hotmail.com>
2025-04-16 10:14:26 +0200

    Implement weapon switching

--- characters/weapons/sword.gd
+++ characters/weapons/sword.gd
@@ -2,10 +2,15 @@ extends "res://characters/weapons/weapon.gd"
 
 var is_initial_pos = true
 func attack():
+	var anim = $SwingAnimation
 	if is_initial_pos:
 		$AnimationPlayer.play("swing_to")
 	else:
 		$AnimationPlayer.play("swing_fro")
+		
+	anim.scale = Vector2(anim.scale.x, anim.scale.y * -1)
+	$SwingAnimation.stop()
+	$SwingAnimation.play("swing")
 	is_initial_pos = not is_initial_pos
 	
 	super.attack()
@@ -13,4 +18,4 @@ func attack():
 func _on_hurtbox_body_entered(body: Node2D) -> void:
 	if $AnimationPlayer.is_playing():
 		if body.is_in_group("Enemy") and body.has_method("take_damage"):
-			body.take_damage(damage, self)
+			body.take_damage(damage, null, Vector2.RIGHT.rotated(global_rotation))
---
+++ weapon_indicator.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var weapon_type: String = "sword"
+
+func _ready():
+	if not weapon_type:
+		return
+		
+	for child in $WeaponSprite.get_children():
+		child.visible = false
+	if weapon_type == "sword":
+		$WeaponSprite/SwordSprite.visible = true
+	elif weapon_type == "gun":
+		$WeaponSprite/GunSprite.visible = true
+		
+func change_weapon(type):
+	if type == weapon_type:
+		$ActiveBackground.visible = true
+		$InactiveBackground.visible = false
+		$WeaponSprite.modulate.a = 1.0
+	else:
+		$ActiveBackground.visible = false
+		$InactiveBackground.visible = true
+		$WeaponSprite.modulate.a = 0.5
