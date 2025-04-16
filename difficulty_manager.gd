c49345bb5cf928c34695d1121694cf6f31c32f91
Maurice <mauricetulp@live.nl>
2025-04-16 14:38:29 +0200

    Sounds connect pipe and salt is full

--- difficulty_manager.gd
+++ difficulty_manager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var difficulty_stages: Array[int] = [0]
+var succeeded;
 
 func _ready() -> void:
 	$BackgroundMusicExploration.play()
@@ -11,6 +12,13 @@ func add_difficulty_stage(stage: int):
 	if stage > 0 && !$BackgroundMusicActionFight.playing:
 		$BackgroundMusicExploration.stop()
 		$BackgroundMusicActionFight.play()
+	
+	if stage == 1:
+		$ConnectPipeSound.play()
+		
+	if stage == 2 && !succeeded:
+		$SuccesSaltSound.play()
+		succeeded = true
 	
 func get_difficulty_stages() -> Array[int]:
 	return difficulty_stages
