31f2061f2dc37df16700d7b8f736504cceab169f
Laurens Neinders <Laurens.neinders@codec.nl>
2025-04-18 13:49:51 +0200

    Start with controller "a" on start menu

--- start_menu.gd
+++ start_menu.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/level1.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pickup"):
+		_on_texture_button_pressed()
